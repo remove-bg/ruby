3f6785a8b1ccf4c24037316f606c6d072a14504e
Oliver Peate <oliverp@gmail.com>
2019-04-03 10:25:25 +0100

    Add bulk process example

---
+++ examples/bulk_process.rb
@@ -0,0 +1,26 @@
+require "remove_bg"
+require "fileutils"
+
+# Usage:
+# REMOVE_BG_API_KEY=api-key bundle exec ruby bulk_process.rb
+
+api_key = ENV.fetch("REMOVE_BG_API_KEY")
+
+# Create output directory if it doesn't exist
+output_directory = File.expand_path("output", __dir__)
+FileUtils.mkdir_p(output_directory)
+
+# Find all JPGs & PNGs in input directory
+input_images = Dir.glob(File.expand_path("input/*.{jpg,png}", __dir__))
+puts "Found #{input_images.count} images to process..."
+
+# Process each image and save to the output directory
+input_images.each do |input_image|
+  output_path = File.join(output_directory, File.basename(input_image))
+
+  RemoveBg
+    .from_file(input_image, api_key: api_key, size: "regular")
+    .save(output_path, overwrite: true)
+
+  puts "✅ Processed #{input_image} -> #{output_path}"
+end
