82442d7caff8844f40c4473f33abe2b105a3b2d0
Oliver Peate <oliverp@gmail.com>
2020-05-26 15:22:46 +0100

    Rename option to 'auto_upgrade_png_to_zip'

--- lib/remove_bg/configuration.rb
+++ lib/remove_bg/configuration.rb
@@ -2,14 +2,14 @@ require_relative "image_composer"
 
 module RemoveBg
   class Configuration
-    attr_accessor :api_key, :image_processor, :save_bandwidth
+    attr_accessor :api_key, :image_processor, :auto_upgrade_png_to_zip
 
     def self.configuration
       @configuration ||= Configuration.new.tap do |config|
         config.image_processor = ImageComposer.detect_image_processor
 
         # Upgrade to ZIP where possible to save bandwith
-        config.save_bandwidth = true
+        config.auto_upgrade_png_to_zip = true
       end
     end
 
