12f519eec0211e8851b93167c6b38a56753f72f4
Alexander Graf <ag@canva.com>
2024-04-22 17:54:01 +0200

    Remove deprecated codecov uploader

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -5,9 +5,3 @@ require "simplecov"
 SimpleCov.start do
   add_filter "/spec/"
 end
-
-if ENV.key?("UPLOAD_COVERAGE")
-  require "codecov"
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::SimpleFormatter, SimpleCov::Formatter::Codecov])
-end
