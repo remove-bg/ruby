ab0ac9d64f4ccf893414f35c95c77b0d28464843
Alexander Graf <ag@canva.com>
2024-04-22 18:06:49 +0200

    Add cobertura output for codecov

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require "simplecov"
+require "simplecov-cobertura"
 
 SimpleCov.start do
   add_filter "/spec/"
 end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::SimpleFormatter, SimpleCov::Formatter::CoberturaFormatter])
